ec3d104937eae68d9e86aebd760740fd6e2ef8ca
addy <addyyy118@gmail.com>
2024-07-04 17:05:49 +0530

    Fetched Orders

--- scripts/checkout/paymentSummary.js
+++ scripts/checkout/paymentSummary.js
@@ -2,6 +2,7 @@ import cart from '../../data/cartClass.js'
 import { getDeliveryOption } from '../../data/deliveryOptions.js'
 import {getProduct} from "../../data/products.js";
 import { formatCurrency } from "../utils/money.js";
+import {addOrder} from "../../data/orders.js";
 
 export function renderPaymentSummary() {
 	let productPriceCents = 0;
@@ -49,10 +50,32 @@ export function renderPaymentSummary() {
       <div class="payment-summary-money">$${formatCurrency(totalCents)}</div>
     </div>
 
-    <button class="place-order-button button-primary">
+    <button class="place-order-button button-primary js-place-order">
       Place your order
     </button>
 	`;
 	
 	document.querySelector('.js-payment-summary').innerHTML = paymentSummaryHTML;
+	
+	document.querySelector('.js-place-order')
+		.addEventListener('click', async () => {
+			try {
+				const response = await fetch('https://supersimplebackend.dev/orders', {
+					method: 'POST',
+					headers: {
+						'Content-Type': 'application/json'
+					},
+					body: JSON.stringify({
+						cart: cart.cartItems
+					})
+				});
+				
+				const order = response.json();
+				addOrder(order);
+				
+				window.location.href = 'orders.html'
+			} catch {
+				console.log('Unexpected Error! Please try again later.')
+			}
+		})
 }
